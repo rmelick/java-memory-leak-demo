c7541d229b7aa6f40dc28ec2a255717826953fb7
Russell Melick <russell.melick@kiwigrid.com>
2017-02-27 14:33:36 +0100

    Update description

--- src/main/java/com/github/rmelick/memory/app/App.java
+++ src/main/java/com/github/rmelick/memory/app/App.java
@@ -5,12 +5,12 @@ import com.github.rmelick.memory.api.DeviceService;
 import com.github.rmelick.memory.api.DeviceUpdateMessages;
 import com.github.rmelick.memory.service.DeviceServiceImpl;
 import io.vertx.core.Vertx;
-import io.vertx.core.VertxOptions;
 import io.vertx.core.http.HttpServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class App {
     private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
@@ -24,11 +24,13 @@ public class App {
         LOGGER.info("Starting application");
         final DeviceService deviceService = new DeviceServiceImpl();
 
+        AtomicLong requestCounter = new AtomicLong(0);
         Vertx vertx = Vertx.vertx();
         HttpServer httpServer = vertx.createHttpServer();
         httpServer.requestHandler(request -> {
             request.bodyHandler(body -> {
                 try {
+                    LOGGER.info("Received request " + requestCounter.incrementAndGet());
                     DeviceUpdateMessages parsedMessage = OBJECT_MAPPER.readValue(body.toString(), DeviceUpdateMessages.class);
                     deviceService.receiveMessage(parsedMessage);
                     request.response().end();
